5323350ae7ddb8b7c1b9c68e28ec391f05498734
YuraRov <yura.rov555@gmail.com>
2022-10-20 09:26:45 +0300

    Finished task №1

---
+++ lab-1/MutexUsing/MutexUsing/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <thread>
+
+// Task 1
+void Thread1()
+{
+	std::cout << 1 << '\n';
+}
+
+// Task 1
+void Thread2()
+{
+	std::cout << 2 << '\n';
+}
+
+int main()
+{
+	// Task 1
+	std::thread first_thread(Thread1);
+	std::thread second_thread(Thread2);
+
+	return 0;
+}
