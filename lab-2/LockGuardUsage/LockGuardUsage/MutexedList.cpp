bb3365c6847969a3b5d77dd403b5e6850db1c047
YuraRov <yura.rov555@gmail.com>
2022-10-20 13:02:46 +0300

    Finished task №3 (1.2.1)

--- lab-2/LockGuardUsage/LockGuardUsage/MutexedList.cpp
+++ lab-2/LockGuardUsage/LockGuardUsage/MutexedList.cpp
@@ -6,23 +6,19 @@
 
 void MutexedList::AddToList(int element_to_insert)
 {
-	mutex_for_data_.lock();
+	std::lock_guard<std::mutex> lock(mutex_for_data_);
 
 	data_.push_back(element_to_insert);
 	std::cout << "Element " << element_to_insert << " was added to list\n";
-
-	mutex_for_data_.unlock();
 }
 
 bool MutexedList::ListContains(int element_to_find)
 {
-	mutex_for_data_.lock();
+	std::lock_guard<std::mutex> lock(mutex_for_data_);
 
 	bool found = std::find(data_.begin(), data_.end(), element_to_find) != data_.end();
 	std::string message = found ? "Element " + std::to_string(element_to_find) + " was found in list"
 								: "Element " + std::to_string(element_to_find) + " wasn't found in list";
 	std::cout << message << '\n';
-
-	mutex_for_data_.unlock();
 	return found;
 }
