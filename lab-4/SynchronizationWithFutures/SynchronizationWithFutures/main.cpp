c0dc9fd6ec398fed2c62e3530df98f6edcb16d9d
YuraRov <yura.rov555@gmail.com>
2022-12-06 22:13:46 +0200

    Finished task №1.2.2

--- lab-4/SynchronizationWithFutures/SynchronizationWithFutures/main.cpp
+++ lab-4/SynchronizationWithFutures/SynchronizationWithFutures/main.cpp
@@ -1,11 +1,16 @@
-#include <array>
+#include <condition_variable>
+#include <deque>
 #include <future>
+#include <mutex>
 #include <iostream>
+#include <utility>
 #include <vector>
 
-const int kMaxPrimeNumber = 15'485'917;
+// ---------------------- TASK 1 BEGIN -------------------------------
+//const int kMaxPrimeNumber = 15'485'917;
+const int kMaxPrimeNumber = 305'597;
 
-std::vector<int> GetPrimeNumbersVector()
+int GetPrimeNumberByIndex(int index)
 {
 	std::vector<bool> is_prime(kMaxPrimeNumber, true);
 
@@ -28,7 +33,7 @@ std::vector<int> GetPrimeNumbersVector()
 			prime_numbers.push_back(p);
 		}	
 	}
-	return prime_numbers;
+	return prime_numbers[index];
 }
 
 void GameWithUser(int enteredIndex)
@@ -52,30 +57,109 @@ void GameWithUser(int enteredIndex)
 		default:
 			break;
 	}
-	std::cout << "OK, we still waiting for the result...\n";
+	std::cout << "OK, we are still waiting for the result...\n";
 }
 
-void Task1DefferedAsync(int enteredPrimeIndex)
+void Task1DefferedAsync()
 {
-	std::future<std::vector<int>> deferred_result = std::async(std::launch::deferred, GetPrimeNumbersVector);
-	GameWithUser(enteredPrimeIndex);
-	std::cout << "The result is " << deferred_result.get()[enteredPrimeIndex - 1] << '\n';
+	int prime_number_index = 1;
+	std::cout << "Enter the index of a prime number (n <= 1000000): ";
+	std::cin >> prime_number_index;
+	std::future<int> deferred_result = std::async(std::launch::deferred, GetPrimeNumberByIndex, prime_number_index - 1);
+	GameWithUser(prime_number_index);
+	std::cout << "The result is " << deferred_result.get() << '\n';
 }
 
-void Task1RealAsync(int enteredPrimeIndex)
+void Task1RealAsync()
 {
-	std::future<std::vector<int>> async_result = std::async(std::launch::async, GetPrimeNumbersVector);
-	GameWithUser(enteredPrimeIndex);
-	std::cout << "The result is " << async_result.get()[enteredPrimeIndex - 1] << '\n';
+	int prime_number_index = 1;
+	std::cout << "Enter the index of a prime number (n <= 1000000): ";
+	std::cin >> prime_number_index;
+	std::future<int> async_result = std::async(std::launch::async, GetPrimeNumberByIndex, prime_number_index - 1);
+	GameWithUser(prime_number_index);
+	std::cout << "The result is " << async_result.get() << '\n';
 }
+
+// ---------------------- TASK 1 END ----------------------------------
+
+// ---------------------- TASK 2 BEGIN --------------------------------
+
+std::deque<std::packaged_task<int(int)>> tasks;
+std::deque<int> prime_number_indexes;
+std::mutex defensive_mutex;
+
+void Calculation()
+{
+	do
+	{
+		std::packaged_task<int(int)> prime_number_task;
+		std::unique_lock<std::mutex> thread_lock(defensive_mutex);
+		
+		prime_number_task = std::move(tasks.front());
+		tasks.pop_front();
+		int prime_number_index = prime_number_indexes.front();
+		prime_number_indexes.pop_front();
+		thread_lock.unlock();
+
+		prime_number_task(prime_number_index - 1);
+	} while (!tasks.empty() && !prime_number_indexes.empty());
+}
+
+void Task2()
+{
+	std::thread independent_thread;
+	int prime_number_index = 1;
+	std::vector<std::future<int>> results;
+	bool start_flag = true;
+	std::cout << "Enter the index of a prime number (n <= 1000000, -1 - to stop): ";
+	std::cin >> prime_number_index;
+	while (prime_number_index != -1)
+	{
+		std::packaged_task<int(int)> prime_number_task(GetPrimeNumberByIndex);
+		results.push_back(std::move(prime_number_task.get_future()));
+
+		std::unique_lock<std::mutex> main_lock(defensive_mutex);
+		tasks.push_back(std::move(prime_number_task));
+		prime_number_indexes.push_back(prime_number_index);
+		main_lock.unlock();
+
+		if (start_flag)
+		{
+			independent_thread = std::thread(Calculation);
+			start_flag = false;
+		}
+		std::cout << "Enter the index of a prime number (n <= 1000000, -1 - to stop): ";
+		std::cin >> prime_number_index;
+	}
+
+	std::cout << "Your results: ";
+
+	for (size_t i = 0; i < results.size(); i++)
+	{
+		if (i == results.size() - 1)
+		{
+			std::cout << results[i].get();
+		}
+		else 
+		{
+			std::cout << results[i].get() << ' ';
+		}
+	}
+	std::cout << '\n';
+	independent_thread.join();
+}
+
+// ---------------------- TASK 2 END ----------------------------------
+
+// ---------------------- TASK 3 BEGIN --------------------------------
+
 
 int main()
 {
-	int primeNumberIndex = 1;
-	std::cout << "Enter the index of a prime number that you want to know (n <= 1000000): ";
-	std::cin >> primeNumberIndex;
-	//Task1DefferedAsync(primeNumberIndex);
-	Task1RealAsync(primeNumberIndex);
+	//Task1DefferedAsync();
+	//Task1RealAsync();
+	Task2();
+
 	system("pause");
 	return 0;
 }
